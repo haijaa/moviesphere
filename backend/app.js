954de7b216e4d46915fc94c730d746b336c2b889
FridaWikman <frida.wikman@hotmail.com>
2024-04-24 14:34:05 +0200

    lagt till movieDescription

--- backend/app.js
+++ backend/app.js
@@ -1,20 +1,20 @@
-const express = require("express");
-const cors = require("cors");
+const express = require('express')
+const cors = require('cors')
 
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
-app.use(cors());
-app.use(express.static("public"));
+app.use(cors())
+app.use(express.static('public'))
 
-const actorRoutes = require("./routes/actorRoutes");
-const movieRoutes = require("./routes/movieRoutes");
+const actorRoutes = require('./routes/actorRoutes')
+const movieRoutes = require('./routes/movieRoutes')
 
-app.use(actorRoutes);
-app.use(movieRoutes);
+app.use(actorRoutes)
+app.use(movieRoutes)
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
