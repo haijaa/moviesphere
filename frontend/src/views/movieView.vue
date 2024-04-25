cd89762968c58db63a849394f71709bcdd6facef
FridaWikman <frida.wikman@hotmail.com>
2024-04-25 09:26:23 +0200

    lägger till router.js och movieView.vue

---
+++ frontend/src/views/movieView.vue
@@ -0,0 +1,42 @@
+<script setup>
+import { ref } from 'vue';
+
+const movies = ref([])
+const directors = ref([])
+
+
+function fetchMovies() {
+fetch('http://localhost:3000/movies')
+.then(response => response.json())
+.then(data => {
+  movies.value = data
+})
+  }
+fetchMovies()
+
+function fetchDirectors() {
+fetch('http://localhost:3000/directors')
+.then(response => response.json())
+.then(data => {
+  directors.value = data
+})
+  }
+
+  fetchDirectors()
+
+</script>
+<template>
+<main>
+  <h1>MOVIES</h1>
+  <div>
+  <ul>
+  <li v-for="movie in movies"> {{ movie.movieTitle }} </li>
+</ul>
+</div>
+<div>
+  <ul>
+  <li v-for="director in directors">{{ director.directorImg }} </li>
+</ul>
+</div>
+</main>
+</template>
